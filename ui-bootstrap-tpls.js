787666aa0bbf6f1b636b8e3e15b151c5e6b501b8
SouLsKi <soulski@gmail.com>
2013-09-11 10:55:50 +0700

    add modal template

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -3412,6 +3412,20 @@ angular.module("template/datepicker/popup.html", []).run(["$templateCache", func
     "</ul>");
 }]);
 
+angular.module("template/modal/backdrop.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("template/modal/backdrop.html",
+  	"<div class=\"modal-backdrop fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1040 + index*10}\">\n" + 
+  	"</div>\n" +
+    "");
+}]);
+
+angular.module("template/modal/window.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("template/modal/window.html",
+  	"<div class=\"modal fade {{ windowClass }}\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1050 + index*10, display: 'block'}\" ng-click=\"close($event)\">\n" +
+    	"<div class=\"modal-dialog\"><div class=\"modal-content\" ng-transclude></div></div>\n" +
+		"</div>\n" +
+		"");
+}]);
 angular.module("template/pagination/pager.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/pagination/pager.html",
     "<ul class=\"pager\">\n" +
